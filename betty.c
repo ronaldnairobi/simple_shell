86360d05399fb57abc5820bd0a7107b627d085e2
ronaldnairobi <ronaldjnr3@gmail.com>
2023-11-10 07:26:34 -0800

    my first commit

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A simple program that prints "Hello, Betty!" to the console.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Hello, Betty!\n");
+return (0);
+}
